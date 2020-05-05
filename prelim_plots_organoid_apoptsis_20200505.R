a7e18e4db88c4ddd7d88fcdb7bbda40677fa66a3
bdemarest <bradleydemarest@gmail.com>
2020-05-05 15:02:14 -0600

    Initial commit

---
+++ prelim_plots_organoid_apoptsis_20200505.R
@@ -0,0 +1,9 @@
+# Apoptosis data from timelapse green fluorescent imaging of brain organoids,
+# ~20 hours (5 minute time steps) after transfer to matrigel.
+
+# Experiment design:
+# 3 genotypes - WT (WTC11), KMT2D null (KMT2D5.6), KMT2D null (KMT2D10.1)
+# null lines were separately derived, but using the same CRISPR guide rna.
+
+
+
